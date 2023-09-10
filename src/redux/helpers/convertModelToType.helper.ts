f0f319040eaf4f4eb06b0ad460e0830b78b9f3bf
DavidR <codingaccelerator@gmail.com>
2023-09-10 21:54:19 +0200

    🎨  F01: SLICES > Move helpers folder from slices to redux

--- src/redux/helpers/convertModelToType.helper.ts
+++ src/redux/helpers/convertModelToType.helper.ts
@@ -1,5 +1,5 @@
-import { ICocktailModel } from "../../../api/models";
-import { ICocktail } from "../cocktail/cocktailTypes";
+import { ICocktailModel } from "../../api/models";
+import { ICocktail } from "../slices/cocktail/cocktailTypes";
 
 export const convertModelToCocktailType = (
   cocktail: ICocktailModel
